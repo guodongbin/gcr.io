0757972dffc6eac78c96ce73865f7cecf9116a22
guodongbin <guodongbin@users.noreply.github.com>
2018-05-29 10:52:47 +0800

    Create cup.js

---
+++ cup.js
@@ -0,0 +1,19 @@
+{
+	"browserWindowConfig":{
+		"width":1400,
+		"height":900,
+		"fullscreen":false,
+		"alwaysOnTop": true,
+        "minimizable": false,
+		"closable":true,
+		"resizable":false,
+		"webPreferences": {
+	  		"webSecurity": false,
+			"nodeIntegration":false
+	  	}
+	},
+	"otherConfig":{
+		"url":"https://ecs.qmth.com.cn:8878/oe/#/access/login?domain=cup.ecs.qmth.com.cn#top",
+		"openDevTool":false
+	}
+}
